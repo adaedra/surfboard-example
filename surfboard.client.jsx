ed3c20f0919759e3911342a7d78033b7670fb2c3
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-01-17 20:43:56 +0100

    Adapt to latest changes

--- surfboard.client.jsx
+++ surfboard.client.jsx
@@ -1,20 +1,15 @@
 import { initialize } from '@surfboard/ui'
 import Widget from '@surfboard/ui/dist/widget'
-import dispatcher from '@surfboard/ui/dist/websocket'
+import { subscribeTo } from '@surfboard/ui/dist/websocket'
 import React from 'react'
-import { pluck } from 'rxjs/operators'
 
 class Dummy extends Widget {
-    constructor(props) {
-        super(props)
-
-        this.sources = {
-            value: dispatcher.pipe(pluck(this.props.source))
-        }
+    subscribe() {
+        return subscribeTo(this.props.source)
     }
 
     render() {
-        return <div>{this.state.value}</div>
+        return <div>{this.state.dummy}</div>
     }
 }
 
