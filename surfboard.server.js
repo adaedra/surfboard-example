4540abfad69bfe0c9bd761a35ed1dce7877a78ea
Thibault “Adædra” Hamel <contact@adaedra.eu>
2019-02-11 22:12:59 +0100

    Add an example HTTP backend

--- surfboard.server.js
+++ surfboard.server.js
@@ -1,7 +1,14 @@
 const { interval } = require('rxjs')
+const express = require('express')
 
 const sources = {
     dummy: interval(1000)
 }
 
-module.exports = { sources }
+const server = express()
+
+server.get('/foo', (_req, res) => {
+    res.send({ foo: 'bar' })
+})
+
+module.exports = { sources, server }
